5257fac045980775c042c16dde5bdcdd58ad0ada
Jayanth Chornur Srinivasa <chornur.srinivasa.jayanth@volvo.com>
2018-03-30 11:12:01 +0530

    concepts

---
+++ general-concepts-theory/concepts-angular-basic.js
@@ -0,0 +1,10 @@
+// {{storyobj | json}}
+
+//* represent structural directive --> can change the str of a page.
+
+// in v/s of
+
+// interpolation , 1 way , 2 way, event binding
+
+// banana in a box -> property name : 1st way
+																						// event braces : 2nd way
